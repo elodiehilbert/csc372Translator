3f1780e1a6728369be398c93618ed1136d1636e8
Lucas Tuan <lucastuan@gmail.com>
2024-04-21 15:18:18 -0700

    Added the ability to open files to the interactive thing

--- Interactive.py
+++ Interactive.py
@@ -1,7 +1,12 @@
 from translator import translate_line_by_line
+import re
 
-print("Type 'exit' to leave")
+print("Type 'exit' to leave and open(\"File_name\") to run a file")
 user = input()
 while(user != "exit"):
-    exec(translate_line_by_line(user))
+    if(re.match(r'open\("([^"]+)"\)', user)):
+        fileName = re.match(r'open\("([^"]+)"\)', user).group(1)
+        exec(translate_line_by_line(open(fileName, "r").read()))
+    else:
+        exec(translate_line_by_line(user))
     user = input()
