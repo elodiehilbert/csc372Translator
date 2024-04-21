b50eef8fe59d0e11877aa97236fcef27a1e4f633
Lucas Tuan <lucastuan@gmail.com>
2024-04-21 15:47:46 -0700

    Fixed indentations and removed unused code

--- translator.py
+++ translator.py
@@ -1,4 +1,3 @@
-from old_translator import translate_to_python
 import random
 import string
 import re
@@ -17,115 +16,115 @@ curry_pattern = r'^\s*(f\w+)\s*([^(){]*)\s*{'
 curry_call = r'(f\w+)\s*([\w ]*)'
 
 def translate_line_by_line(code):
-        lines = code.split("\n")
-        indent = 0
-        output = ""
-        extra = 0
-        endings = []
-        for line in lines:
-                # Remove indents if it is ending an indented block thing
-                if line.find("}") != -1:
-                        line = line.replace("}", "")
-                        if extra > 0 and endings[len(endings) - 1] != "":
-                                for j in range(extra):
-                                        indent -= 1
-                                        index = len(endings) - 1
-                                        output += ("\t" * indent) + endings[index] + "\n"
-                                        endings.remove(endings[index])
-                                extra = 0
-                                indent -= 1
-                        else:
-                                index = len(endings) - 1
-                                output += ("\t" * indent) + endings[index] + "\n"
-                                endings.remove(endings[index])
-                                indent -= 1                
+    lines = code.split("\n")
+    indent = 0
+    output = ""
+    extra = 0
+    endings = []
+    for line in lines:
+        # Remove indents if it is ending an indented block thing
+        if line.find("}") != -1:
+            line = line.replace("}", "")
+            if extra > 0 and endings[len(endings) - 1] != "":
+                for j in range(extra):
+                        indent -= 1
+                        index = len(endings) - 1
+                        output += ("\t" * indent) + endings[index] + "\n"
+                        endings.remove(endings[index])
+                extra = 0
+                indent -= 1
+            else:
+                index = len(endings) - 1
+                output += ("\t" * indent) + endings[index] + "\n"
+                endings.remove(endings[index])
+                indent -= 1                
 
-                # Add indents to start of line
-                output += "\t" * indent
-        
-                # Check for unclosed string
-                if (line.count("\"") - line.count("\\\"")) % 2 != 0:
-                     raise SyntaxError("Unclosed string at\n" + line)
+        # Add indents to start of line
+        output += "\t" * indent
 
-                # Catch invalid assignments:
-                if(re.match(invalid_assignment_pattern, line)):
-                     raise SyntaxError("Invalid variable assignment, use 'is' instead of '=' at\n" + line)
+        # Check for unclosed string
+        if (line.count("\"") - line.count("\\\"")) % 2 != 0:
+            raise SyntaxError("Unclosed string at\n" + line)
 
-                # Translate assignments
-                line = re.sub(assignment_pattern, lambda m: translate_assignment(m), line)
+        # Catch invalid assignments:
+        if(re.match(invalid_assignment_pattern, line)):
+            raise SyntaxError("Invalid variable assignment, use 'is' instead of '=' at\n" + line)
 
-                # Translate variables
-                line = re.sub(variable_pattern, lambda m: translate_variable(m), line)
+        # Translate assignments
+        line = re.sub(assignment_pattern, lambda m: translate_assignment(m), line)
 
-                # # Translate output and input
-                line = re.sub(output_pattern, lambda m: translate_output(m), line)
-                line = re.sub(input_pattern, lambda m: translate_input(m), line)
+        # Translate variables
+        line = re.sub(variable_pattern, lambda m: translate_variable(m), line)
 
-                # Translate while loops
-                if(re.search(while_loop_pattern, line)):
-                        line = re.sub(while_loop_pattern, lambda m: translate_while_loop(m), line)
-                        endings.append("")
-                        indent += 1
+        # # Translate output and input
+        line = re.sub(output_pattern, lambda m: translate_output(m), line)
+        line = re.sub(input_pattern, lambda m: translate_input(m), line)
 
-                # Translate for loop
-                if(re.search(for_loop_pattern, line)):
-                        init, condition, every = re.search(for_loop_pattern, line).groups()
-                        real_line = init + "\n" + ("\t" * indent)
-                        real_line += "while " + condition + ":"
-                        line = real_line
-                        endings.append(every)
-                        indent += 1
+        # Translate while loops
+        if(re.search(while_loop_pattern, line)):
+            line = re.sub(while_loop_pattern, lambda m: translate_while_loop(m), line)
+            endings.append("")
+            indent += 1
 
-                # Translate if statements
-                if(re.search(if_smt_pattern, line)):
-                        line = re.sub(if_smt_pattern, lambda m: translate_if_smt(m), line)
-                        endings.append("")
-                        indent += 1
+        # Translate for loop
+        if(re.search(for_loop_pattern, line)):
+            init, condition, every = re.search(for_loop_pattern, line).groups()
+            real_line = init + "\n" + ("\t" * indent)
+            real_line += "while " + condition + ":"
+            line = real_line
+            endings.append(every)
+            indent += 1
 
-                # Translate else statements
-                if(re.search(else_smt_pattern, line)):
-                        line = re.sub(else_smt_pattern, lambda m: translate_else_smt(m), line)
-                        endings.append("")
-                        indent += 1
+        # Translate if statements
+        if(re.search(if_smt_pattern, line)):
+            line = re.sub(if_smt_pattern, lambda m: translate_if_smt(m), line)
+            endings.append("")
+            indent += 1
 
-                # Translate function heads
-                if(re.search(function_pattern, line)):
-                        line = re.sub(function_pattern, lambda m: translate_function_head(m), line)
-                        endings.append("")
-                        indent += 1
+        # Translate else statements
+        if(re.search(else_smt_pattern, line)):
+            line = re.sub(else_smt_pattern, lambda m: translate_else_smt(m), line)
+            endings.append("")
+            indent += 1
 
-                # Translate curry function heads
-                if(re.search(curry_pattern, line)):
-                        name, args = re.search(curry_pattern, line).groups()
-                        args = args.split()
-                        real_line = "\t" * indent
-                        indent += 1
-                        if(len(args) == 0):
-                            real_line += f'def {name}():'
-                            endings.append("")
-                        else:
-                            real_line += f'def {name}({args[0]}):'
-                            
-                            for j in range(1, len(args)):
-                                    real_line += '\n' + ("\t" * indent)
-                                    fname = ''.join(random.choices(string.ascii_letters, k = 10))
-                                    real_line += f'def {fname}({args[j]}):'
-                                    endings.append(f'return {fname}')
-                                    extra += 1
-                                    indent += 1
-                        line = real_line
+        # Translate function heads
+        if(re.search(function_pattern, line)):
+            line = re.sub(function_pattern, lambda m: translate_function_head(m), line)
+            endings.append("")
+            indent += 1
 
-                # Translate curry function calls
-                if(re.search(curry_call, line)):
-                        line = re.sub(curry_call, lambda m: translate_curry_call(m), line)
+        # Translate curry function heads
+        if(re.search(curry_pattern, line)):
+            name, args = re.search(curry_pattern, line).groups()
+            args = args.split()
+            real_line = "\t" * indent
+            indent += 1
+            if(len(args) == 0):
+                real_line += f'def {name}():'
+                endings.append("")
+            else:
+                real_line += f'def {name}({args[0]}):'
+                
+                for j in range(1, len(args)):
+                    real_line += '\n' + ("\t" * indent)
+                    fname = ''.join(random.choices(string.ascii_letters, k = 10))
+                    real_line += f'def {fname}({args[j]}):'
+                    endings.append(f'return {fname}')
+                    extra += 1
+                    indent += 1
+            line = real_line
 
-                line = line.lstrip()
-                output += line + "\n"
+        # Translate curry function calls
+        if(re.search(curry_call, line)):
+            line = re.sub(curry_call, lambda m: translate_curry_call(m), line)
 
-        # Check for unclosed brackets
-        if(indent != 0):
-             raise SyntaxError("Unclosed Brackets")
-        return output
+        line = line.lstrip()
+        output += line + "\n"
+
+    # Check for unclosed brackets
+    if(indent != 0):
+            raise SyntaxError("Unclosed Brackets")
+    return output
 
 def translate_assignment(match):
     var_type, var_name, value = match.groups()
@@ -163,7 +162,7 @@ def translate_if_smt(match):
     return f'if {condition}:'
 
 def translate_else_smt(match):
-     return "else:"
+    return "else:"
 
 def translate_output(match):
     if match.group(1):
@@ -180,9 +179,9 @@ def translate_curry_call(match):
         output = name
         args = args.split()
         for arg in args:
-              output+= f'({arg})'
+            output+= f'({arg})'
         return output
 
 def translate_function_head(match):
-     name, args = match.groups()
-     return f'def {name}{args}:'
+    name, args = match.groups()
+    return f'def {name}{args}:'
